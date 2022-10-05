0f4ea348628cad4d8e2a663da1533b0c9ba27ce5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-05 18:10:52 +0900

    GraphQLの導入設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      // schemaファイルのパスを指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // 生成されたschemaを自動でsortされるためのオプションをオンにする
+      sortSchema: true,
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
